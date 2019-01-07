bfdb88db06ea2f5d65ad29c8a825da8dbcf95bc1
Bruno Bonnin <bbonnin@gmail.com>
2019-01-07 16:33:30 +0100

    Update jsonl.js

--- jsonl.js
+++ jsonl.js
@@ -1,1 +1,23 @@
 // JSON Lines parser
+// TODO: npm install papaparse
+const Papa = require('papaparse')
+
+const jsonl = {
+  toJsonl: function (csvFile, options) {
+    // options contains:
+    // - delimiter (default: ,)
+    // - field names (default: the first line contains this information or it just generates an array of values)
+    // - quoteChar
+    
+    // read each line and convert it into json
+    const jsonlines = []
+    blabla.forEach(line => {
+      const json = Papa.parse(line, options)
+      if (!json.errors) {
+        jsonlines.push(JSON.stringify(json.data[0]))
+      }
+    })
+  }
+}
+
+modules.export = jsonl
